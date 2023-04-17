6130fb822ab8c206f146ae06910e6652d4dfd0f7
raphaeldamasceno <rdamasceno1852@gmail.com>
2023-04-16 21:47:15 -0300

    finish

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { GlobalStyle } from './styles/Global'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Footer } from "./components/Footer";
+import { NavBar } from "./components/Navbar";
+import { Home } from "./pages/home";
+import { GlobalStyle } from "./styles/Global";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <GlobalStyle>
-      
+      <NavBar />
+      <Home />
+      <Footer />
     </GlobalStyle>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
